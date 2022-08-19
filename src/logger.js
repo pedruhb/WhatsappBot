d3bd0fdc9ff6c0d19750a7a7ba423c8d0b77720e
Pedro Henrique <pedrohenriquebottoni@gmail.com>
2022-08-18 23:45:24 -0300

    Fix Pino prettyPrint is deprecated

--- src/logger.js
+++ src/logger.js
@@ -1,18 +1,3 @@
-/*import { createLogger, format, transports } from 'winston';
-
-export const logger = createLogger({
-    level: 'info',
-    format: format.combine(
-        format.colorize(),
-        format.padLevels(),
-        format.simple()
-    ),
-    transports: [
-        new transports.File({ filename: '/logs/error.log', level: 'error' }),
-        new transports.Console()
-    ]
-});*/
-
 import P from "pino";
 
-export const logger = P({ level: 'info', prettyPrint: { colorize: true, levelFirst: true, translateTime: 'dd/mm/yyyy, h:MM:ss TT', }, transport: { target: 'pino-pretty', options: { colorize: true } }, })
+export const logger = P({ level: 'info', transport: { target: 'pino-pretty', options: { colorize: true, colorize: true, levelFirst: true, translateTime: 'dd/mm/yyyy, h:MM:ss TT' } }, })
